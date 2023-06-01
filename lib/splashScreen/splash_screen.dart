363973c72ecf7c1d8b7f2bf945087828784a88ac
/omg <oremgitonga22@gmail.com>
2023-06-01 16:25:53 +0300

    start clone app upto splash screen awesome

---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,61 @@
+import "dart:async";
+
+import "package:drivers_app/mainScreens/main_screen.dart";
+import "package:flutter/material.dart";
+
+    class MySplashScreen extends StatefulWidget {
+      const MySplashScreen({Key? key}) : super(key: key);
+
+      @override
+      State<MySplashScreen> createState() => _MySplashScreenState();
+    }
+
+    class _MySplashScreenState extends State<MySplashScreen>
+    {
+
+      startTimer()
+      {
+        Timer(const Duration(seconds: 13), () async {
+          // send user to main screen
+          Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
+        });
+      }
+
+      @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    startTimer();
+  }
+
+      @override
+      Widget build(BuildContext context) {
+        return Material(
+          child: Container(
+            color: Colors.black,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Image.asset("images/logo1.png"),
+
+                  SizedBox(height: 10,),
+
+                  const Text(
+                    "Uber and inDriver Clone App",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+            )
+            
+          ),
+        );
+      }
+    }
